51da4f71a4814774dd68dbfd97db3fa38953481a
Rebecca <bsoza93@gmail.com>
2024-02-19 21:20:14 -0600

    SPI - using adxl345

---
+++ Code/22_spi_adxl345/Debug/Src/main.d
@@ -0,0 +1,21 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/uart.h ../Inc/adxl345.h ../Inc/spi.h
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/rsoza/Documents/coding/c++/stm32/Workshop/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/uart.h:
+../Inc/adxl345.h:
+../Inc/spi.h:
